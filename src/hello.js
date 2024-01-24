c463748a1214ef3903e4528c1bf172f6ae32a5b1
Adi Siswanto <adi15siswato@gmail.com>
2024-01-25 00:10:12 +0700

    update

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.get("/adi", (req, res) => {
+	res.send("Hello Adi!");
+});
+
 app.listen(3000, () => {
 	console.log(`Server started on 3000`);
 });
